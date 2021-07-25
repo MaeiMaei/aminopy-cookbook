bbf4efa515e67fec927907a2df82624369492b41
MaeMae <maemaeiscutegirl@gmail.com>
2021-07-25 11:17:18 +0200

    Updated version value

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Mae's of the Mae's"
 author = "Mae's of the Mae's"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
